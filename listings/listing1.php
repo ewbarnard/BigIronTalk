a558ddab81bff3a84dd6ce0828680e684e10df88
Edward Barnard <ewbarnard@embarqmail.com>
2017-10-03 14:18:19 -0500

    Listing1 annotation

--- listings/listing1.php
+++ listings/listing1.php
@@ -1,10 +1,13 @@
 <?php
+/* "memory" contains transfer sequence; offset; limit; placeholder for below result */
 $memory = [1023, 1048576, 1024, 0];
 
+/* Load local variables; on CRAY-1 these are transfers to CPU registers */
 $sequence = $memory[0];
 $start = $memory[1];
 $length = $memory[2];
 
+/* Pack into a single 64-bit value and store to "memory" */
 $result = $sequence << 48;
 $result |= $start << 24;
 $result |= $length;
